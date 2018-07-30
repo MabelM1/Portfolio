6717839e1fa2ee3a63f285cb39d4bd191105a9c8
Mabel <macmota@hotmail.com>
2018-07-30 15:09:56 -0400

    added bootstrap CDN link. Removed bootstrap download files

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(compression());
-app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
-app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
-app.use("/js", express.static(__dirname + "/node_modules/popper.js/dist"));
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 app.get("/", (req, res) => {
 	res.send("Hello Express!");
