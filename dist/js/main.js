11240113d791627400df2be41718967d5ec4e473
Mabel <macmota@hotmail.com>
2019-10-10 17:13:12 -0400

    updating portfolio

---
+++ dist/js/main.js
@@ -0,0 +1,39 @@
+
+
+(function() {
+	// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("main-nav");
+
+
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+function handleClickLink(event) {
+
+	const section = event.target.dataset.section;		
+	document.getElementById(section).scrollIntoView({ behavior: 'smooth'});
+}
+
+
+document.querySelectorAll('#main-nav a').forEach((elem) => {
+
+  elem.addEventListener('click', handleClickLink);
+});
+
+
+}())
+
