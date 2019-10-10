4fbae72b339393b6458300e85f11995f130f8871
Mabel <macmota@hotmail.com>
2019-10-10 17:32:38 -0400

    serving static files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("dist"));
 app.use(compression());
 
 app.get("/", (req, res) => {
